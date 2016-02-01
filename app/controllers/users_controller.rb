8aa196541c3801336893aa442e17d1609e90ecf9
Waihon Yew <yewwaihon@gmail.com>
2016-02-01 11:07:41 +0800

    Fix Admin Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :require_signin, except: [:new, :create]
   #before_action :require_correct_user, only: [:edit, :update, :destroy]
   before_action :require_correct_user, only: [:edit, :update]
-  before_action :require_admin, only: [destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @users = User.all
--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -1,16 +1,17 @@
 describe "Deleting a user" do
   before do
-    admin = User.create!(user_attributes(admin: true))
+    admin = User.create!(user_attributes(username: "admin", email: "admin@example.com", 
+                                         admin: true))
     sign_in(admin)
+    @user = User.create!(user_attributes(name: "Regular User", username: "regularuser", 
+                                         email: "regularuser@example.com"))
   end
 
   it "destroys the user and redirects to the home page" do
-    # Arrange
-    user = User.create!(user_attributes)
+    # Arrange - N/A
 
     # Action
-    #visit user_path(user)
-    #sign_in(user)
+    visit user_path(@user)
     click_link "Delete Account"
 
     # Assert
@@ -19,30 +20,28 @@ describe "Deleting a user" do
 
     # Action
     visit users_path
-    expect(page).not_to have_text(user.name)
+    expect(page).not_to have_text(@user.name)
   end
 
-  #it "destroys and signs out the user and redirects to the home page" do
-  it "automatically signs out that user" do
-    # Arrange
-    user = User.create!(user_attributes)
+  # Only Admin Users can delate another user so do not automatically 
+  # sign out the Admin User involved
+  #it "automatically signs out that user" do
+  it "does not automatically sign out the Admin User" do
+    # Arrange - N/A
 
     # Action
-    #visit root_path
-    #sign_in(user)
-
-    visit user_path(user)
+    visit user_path(@user)
     click_link "Delete Account"
 
     # Assert
     expect(current_path).to eq(root_path)
     expect(page).to have_text("Account successfully deleted!")
-    expect(page).to have_link("Sign In")
-    expect(page).to have_link("Sign Up")
-    expect(page).not_to have_link("Sign Out")
+    expect(page).not_to have_link("Sign In")
+    expect(page).not_to have_link("Sign Up")
+    expect(page).to have_link("Sign Out")
 
     # Action
     visit users_path
-    expect(page).not_to have_text(user.name)    
+    expect(page).not_to have_text(@user.name)    
   end
 end
